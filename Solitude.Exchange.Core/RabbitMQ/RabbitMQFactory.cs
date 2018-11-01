43ce86df9cf3c8a35fe336ae77748b35e7df92da
zhengbo <277366155@163.com>
2018-11-01 15:09:40 +0800

    新增RabbitMQ实例

---
+++ Solitude.Exchange.Core/RabbitMQ/RabbitMQFactory.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solitude.Exchange.Core.RabbitMQ
+{
+   public  class RabbitMQFactory
+    {
+        private static IConfigurationSection _rabbitMQConfig;
+        private  ConnectionFactory _factory;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public IConfigurationSection RabbitMQConfig
+        {
+            get
+            {
+                if (_rabbitMQConfig == null)
+                {
+                    _rabbitMQConfig = BaseCore.Configuration.GetSection("RabbitMQConfig");
+                }
+                return _rabbitMQConfig;
+            }
+        }
+        public ConnectionFactory Factory
+        {
+            get
+            {
+                if (_factory == null)
+                {
+                    _factory = new ConnectionFactory()
+                    {
+                        HostName = RabbitMQConfig["Host"],
+                        Port=Convert.ToInt32(RabbitMQConfig["Port"]),
+                        UserName = RabbitMQConfig["User"],
+                        Password = RabbitMQConfig["Pwd"]
+                    };
+                }
+                return _factory;
+            }
+        }
+
+        public IConnection Connection
+        {
+            get
+            {
+                if(_connection==null)
+                    _connection=Factory.CreateConnection();
+                return _connection;
+            }
+        }
+        
+        //创建通道
+        public IModel Channel
+        {
+            get
+            {
+                if (_channel == null)
+                    _channel = Connection.CreateModel();
+                return _channel;
+            }
+        }
+
+
+        public void Publish(string exchangeName="",string queueName="",string routeKey="")
+        {
+            //定义队列
+            Channel.QueueDeclare(queueName, false, false, false, null);
+            //队列绑定到交换机
+            Channel.QueueBind(queueName, exchangeName, routeKey, null);
+            Console.WriteLine("\nRabbitMQ连接成功，请输入消息，输入q退出！");
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+
+                var sendBytes = Encoding.UTF8.GetBytes(input);
+                //发布消息
+                Channel.BasicPublish(exchangeName, routeKey, null, sendBytes);
+            } while (input.Trim().ToLower() != "q");
+}
+
+        ~RabbitMQFactory()
+        {
+            if (_channel != null && !_channel.IsClosed)
+                _channel.Close();
+                _channel.Dispose();
+            if (_connection != null && _connection.IsOpen)
+                _connection.Close();
+                _connection.Dispose();
+        }
+    }
+}
---
+++ Solitude.Exchange.RabbitMQ/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Solitude.Exchange.Core.RabbitMQ;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitude.Exchange.RabbitMQ
+{
+    class Program
+    {
+        private static string ExchangeName = "singnalr";
+        private static string QueueName = "queueName";
+        private static string RouteKey = "MyRoute.*";
+        static void Main(string[] args)
+        {
+
+             Task.Run(()=> { new RabbitMQFactory().Publish(ExchangeName, "tomcat2", "MyRoute.*"); });
+            ClientTest("boo1", "tomcat2", "MyRoute.bo");
+            ClientTest("boo2", "tomcat2","my.bo");
+            Console.ReadLine();
+        }
+
+        public static void ClientTest(string cname,string queueName= "tomcat",string routeKey= "MyRoute.#")
+        {
+           
+            //var queueName = "tomcat";
+            var factory = new RabbitMQFactory();
+            factory.Channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct, false, false, null);
+            factory.Channel.QueueDeclare(queueName, false, false, false, null);
+            factory.Channel.QueueBind(queueName,ExchangeName, routeKey, null);
+            var consumer = new EventingBasicConsumer(factory.Channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body);
+                Console.WriteLine($"【{cname}】收到消息：{message}");
+                //确认该消息已被消费
+                factory.Channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            factory.Channel.BasicConsume(queueName, false, consumer);
+            Console.WriteLine($"消费者{cname}已启动");
+        }
+
+    }
+}
